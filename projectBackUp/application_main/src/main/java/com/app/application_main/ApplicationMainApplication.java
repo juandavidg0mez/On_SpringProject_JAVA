440a5a65956caf71dd2b3f31226052a832743fa0
juandavidg0mez <nihlgomez@gmail.com>
2024-08-23 17:36:42 -0500

    Configuration Area

---
+++ projectBackUp/application_main/src/main/java/com/app/application_main/ApplicationMainApplication.java
@@ -0,0 +1,13 @@
+package com.app.application_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationMainApplication.class, args);
+	}
+
+}
---
+++ projectBackUp/application_main/src/test/java/com/app/application_main/ApplicationMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.application_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

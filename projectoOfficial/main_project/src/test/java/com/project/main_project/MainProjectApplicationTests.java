f31c55ac123da01dc0a4afddf3894e3df108bd65
juandavidg0mez <nihlgomez@gmail.com>
2024-08-27 00:30:08 +0200

    Creacion de entiedad pais

---
+++ projectoOfficial/main_project/src/test/java/com/project/main_project/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.main_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
